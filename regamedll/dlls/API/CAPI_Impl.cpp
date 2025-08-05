1dc66bfa0f4aef4cd0ec9f6535c4e7a3b284c18c
Jonatan Matějka <jonatan1024@gmail.com>
2025-08-05 09:38:38 +0200

    Revert "expose CheckWinConditions"
    This reverts commit a9297a583468093a0fc024b8dea3b2216bb8914a.

--- regamedll/dlls/API/CAPI_Impl.cpp
+++ regamedll/dlls/API/CAPI_Impl.cpp
@@ -143,10 +143,6 @@ CGrenade *EXT_FUNC SpawnGrenade_api(WeaponIdType weaponId, entvars_t *pevOwner, 
 	return nullptr;
 }
 
-void EXT_FUNC CheckWinConditions_api() {
-	CSGameRules()->CheckWinConditions();
-}
-
 ReGameFuncs_t g_ReGameApiFuncs = {
 	CREATE_NAMED_ENTITY,
 
@@ -180,7 +176,6 @@ ReGameFuncs_t g_ReGameApiFuncs = {
 	TextureTypePlaySound_api,
 	CreateWeaponBox_api,
 	SpawnGrenade_api,
-	CheckWinConditions_api,
 };
 
 GAMEHOOK_REGISTRY(CBasePlayer_Spawn);
